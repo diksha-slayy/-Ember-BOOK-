75331f2773197cd0db7ffc925faafd591f078829
Diksha! <149357185+diksha-slayy@users.noreply.github.com>
2024-05-20 19:35:42 +0530

    Delete dproject directory

--- dproject/app/form.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import food
-
-class foodForm(ModelForm):
-    class Meta:
-        model=food
-        fields='__all__'
-
-#this is second way of crud means build in forms
